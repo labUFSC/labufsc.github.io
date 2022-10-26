d0044285b0872cb452885fbbd548f0945a94b290
labUFSC <114488533+labUFSC@users.noreply.github.com>
2022-10-26 14:04:39 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<?php
+ 
+$people_json = file_get_contents('people.json');
+ 
+$decoded_json = json_decode($people_json, false);
+ 
+echo $decoded_json->name;
+// Monty
+ 
+echo $decoded_json->email;
+// monty@something.com
+ 
+echo $decoded_json->age;
+// 77
+ 
+?>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
